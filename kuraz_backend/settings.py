da8546399e72ad8f82ef7f7856a4075eb4f560e3
tsitiii <ziontaa9@gmail.com>
2025-09-04 04:07:06 +0300

    allwoed host

--- kuraz_backend/settings.py
+++ kuraz_backend/settings.py
@@ -17,7 +17,11 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'advanced-budget-tracker-api.onrender.com',
+    'localhost',
+    '127.0.0.1',
+]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
